de2f87c427893c42435366413d4c94ca80ee441d
Megodwin Varghese <mvarghese10@myseneca.ca>
2024-05-30 14:25:26 -0400

    completed lab3a import script

---
+++ lab3a-alrternative.py
@@ -0,0 +1,12 @@
+'''text = lab3a.return_text_value()'''
+
+'''import lab3a
+text = lab3a.return_text_value()
+print(text)
+lab3a.return_number_value()'''
+
+
+import lab3a
+text = lab3a.return_text_value()
+print(text)
+print(lab3a.return_number_value())
