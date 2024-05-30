3ade3ec688f286b5ef9bca9a817b5965ac3049e0
Megodwin Varghese <mvarghese10@myseneca.ca>
2024-05-30 14:23:00 -0400

    completed lab3a

---
+++ lab3a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#return_text_value() function
+''' Author ID: mvarghese10 '''
+
+def return_text_value():
+    name = 'Terry'
+    greeting = 'Good Morning ' + name
+    return greeting
+return_text_value()
+
+# return_number_value() function
+
+def return_number_value():
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return num3
+return_number_value()
+
+
+
+# Main Program
+
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
